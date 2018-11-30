adbee4fbefd5995a08dc7db0f8714455eb88fe8c
Zamiz <zamiztobar@gmail.com>
2018-11-30 15:26:48 -0300

    *Ejercicio 5

--- Ejercicio5.rb
+++ Ejercicio5.rb
@@ -1,24 +1,26 @@
-# Transformar la clase Herviboro en un módulo. Implementar el módulo en la clase Conejo mediante Mixin para poder acceder al método dieta desde la instancia. Finalmente imprimir la definición de Hervíboro.
+# Transformar la clase Herviboro en un módulo.
+# Implementar el módulo en la clase Conejo mediante Mixin para poder acceder al método dieta desde la instancia.
+# Finalmente imprimir la definición de Hervíboro.
 
 module Herviboro
   @@definir = 'Sólo me alimento de vegetales!'
-
   def self.definir
-    @@definir
+    puts @@definir
   end
 
   def dieta
-    "Soy un Herviboro!"
+    print "Soy un Hervíboro!\n"
   end
 end
 
 class Animal
   def saludar
-    "Soy un animal!"
+    print "Soy un animal!\n"
   end
 end
 
-class Conejo < Animal < Herviboro
+class Conejo < Animal
+  include Herviboro
   def initialize(name)
     @name = name
   end
